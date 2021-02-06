40bd6a41da85cb6ee771461cf5c8c7604653abc9
Borguuh <abdrahmanadewale@gmail.com>
2021-02-06 09:07:36 +0100

    adding session authentication

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
 // Load User Model
@@ -29,8 +31,26 @@ app.get('/', (req, res) => {
   res.send('It Works!');
 });
 
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 // Use Routes
 app.use('/auth', auth);
+
 
 const port = process.env.PORT || 5000;
 
