e584f7c06aa0da00b40ac1ec205055e1bd223fb5
Borguuh <abdrahmanadewale@gmail.com>
2021-02-27 12:39:33 +0100

    mongoose err fix

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ const {
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 mongoose.connect(keys.mongoURI, {
-  useMongoClient:true
+  useMongoClient:true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
